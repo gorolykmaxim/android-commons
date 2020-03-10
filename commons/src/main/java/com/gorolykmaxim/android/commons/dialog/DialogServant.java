fad87d38331c7da9eed10fc92160206e4ec6498d
gorolykmaxim <streetbattle52@gmail.com>
2020-03-10 16:55:08 +0200

    Make dialog servant support not only dialog fragments

--- commons/src/main/java/com/gorolykmaxim/android/commons/dialog/DialogServant.java
+++ commons/src/main/java/com/gorolykmaxim/android/commons/dialog/DialogServant.java
@@ -1,12 +1,14 @@
 package com.gorolykmaxim.android.commons.dialog;
 
+import android.app.Dialog;
+
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 
 /**
- * A servant of {@link DialogFragment} instances.
+ * A servant of various dialog types.
  */
-public class DialogFragmentServant {
+public class DialogServant {
     /**
      * Display the specified {@link DialogFragment} using the specified {@link FragmentManager}.
      *
@@ -18,6 +20,19 @@ public class DialogFragmentServant {
     public void showIfNotShown(DialogFragment dialogFragment, FragmentManager fragmentManager) {
         if (!dialogFragment.isAdded()) {
             dialogFragment.show(fragmentManager, null);
+        }
+    }
+
+    /**
+     * Display the specified {@link Dialog}.
+     *
+     * <p>If this dialog is already being displayed, this call will be ignored.</p>
+     *
+     * @param dialog dialog to display
+     */
+    public void showIfNotShown(Dialog dialog) {
+        if (!dialog.isShowing()) {
+            dialog.show();
         }
     }
 }
